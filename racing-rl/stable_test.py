0b5730394ebfb972ee3371abf4f38204d22e547d
megalphian <megnath.ramesh@gmail.com>
2024-11-04 10:34:02 -0500

    Test TD3

--- racing-rl/stable_test.py
+++ racing-rl/stable_test.py
@@ -1,26 +1,26 @@
 import gymnasium as gym
 import numpy as np
 
-from stable_baselines3 import DDPG
+from stable_baselines3 import TD3
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
 env = gym.make("Pendulum-v1", render_mode="rgb_array")
 
-# The noise objects for DDPG
+# The noise objects for TD3
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
+model = TD3("MlpPolicy", env, action_noise=action_noise, verbose=1)
 model.learn(total_timesteps=10000, log_interval=10)
-model.save("ddpg_pendulum")
+model.save("td3_pendulum")
 vec_env = model.get_env()
 
 del model # remove to demonstrate saving and loading
 
-model = DDPG.load("ddpg_pendulum")
+model = TD3.load("td3_pendulum")
 
 obs = vec_env.reset()
 while True:
     action, _states = model.predict(obs)
     obs, rewards, dones, info = vec_env.step(action)
-    env.render("human")
+    vec_env.render("human")
